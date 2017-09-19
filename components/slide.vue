bcc2a1ba3df44a4bb598f413f880bc3b0b09db28
Rabin Julien <julienr114@gmail.com>
2017-09-19 17:51:44 +0200

    add animation when navigate to other section

--- components/slide.vue
+++ components/slide.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
-  section(:id="id", v-show="isVisble")
-    slot
+  transition(:name="transition")
+    section(:id="id", v-show="isVisble")
+      slot
 </template>
 
 <script>
@@ -14,6 +15,11 @@
     computed: {
       isVisble () {
         return this.$store.state.slides[this.$store.state.currentSlideIndex] === this.id
+      },
+      transition () {
+        if (this.$store.state.direction) {
+          return 'slide-' + this.$store.state.direction
+        }
       }
     }
   }
@@ -23,5 +29,46 @@
   section {
     height: 100vh;
     display: flex;
+  }
+
+  .slide-top-enter-active {
+    animation: slideTopIn 1s;
+  }
+  .slide-top-leave-active {
+    animation: slideTopOut 1s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+  }
+  @keyframes slideTopIn {
+    from { transform: translateY(-100%); }
+    to { transform: translateX(0); }
+  }
+  @keyframes slideTopOut {
+    from { transform: translateY(0); }
+    to { transform: translateY(100%); }
+  }
+  .slide-bottom-enter-active {
+    animation: slideBottomIn .7s ease;
+  }
+  .slide-bottom-leave-active {
+    animation: slideBottomOut .7s ease;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+  }
+  @keyframes slideBottomIn {
+    from { transform: translateY(100%); }
+    to { transform: translateX(0); }
+  }
+  @keyframes slideBottomOut {
+    from { transform: translateY(0); }
+    to { transform: translateY(-100%); }
   }
 </style>
--- store/index.js
+++ store/index.js
@@ -4,20 +4,24 @@ const createStore = () => {
   return new Vuex.Store({
     state: {
       slides: [],
-      currentSlideIndex: 0
+      currentSlideIndex: 0,
+      direction: null
     },
     mutations: {
       setSlide (state, slides) {
         state.slides = slides
       },
       toSlide (state, slideIndex) {
+        state.currentSlideIndex < slideIndex ? state.direction = 'bottom' : state.direction = 'top'
         state.currentSlideIndex = slideIndex
       },
       nextSlide (state) {
         state.currentSlideIndex++
+        state.direction = 'bottom'
       },
       prevSlide (state) {
         state.currentSlideIndex--
+        state.direction = 'top'
       }
     }
   })
